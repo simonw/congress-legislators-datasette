2d382833e00c6f8c98a037382bbbd205dddce31c
Simon Willison <swillison@gmail.com>
2022-02-25 11:07:52 -0800

    Add datasette-pretty-json plugin

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ datasette publish cloudrun legislators.db \
   --service congress-legislators \
   --metadata metadata.yml \
   --install datasette-graphql \
-  --install datasette-cluster-map
+  --install datasette-cluster-map \
+  --install datasette-pretty-json
